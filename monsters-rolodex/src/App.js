ad2157db59c30e1f0a920a21dcfde24ca8282876
”0xAureoleNode“ <aureolenode@gmail.com>
2022-09-20 11:52:30 +0800

    commit about setState callBack function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,17 +6,29 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'redux',
+      name: { firstName: 'Yihua', lastName: 'Zhang' },
+      company: 'basecamp',
     };
   }
   render() {
     return (
       <div className="App">
-        <h4>hi, {this.state.name}</h4>
+        <h4>
+          hi, {this.state.name.firstName}
+          {this.state.name.lastName}, I work at {this.state.company}
+        </h4>
         <button
           onClick={() => {
-            this.setState({ name: 'react' });
-            console.log(this.state);
+            this.setState(
+              () => {
+                return {
+                  name: { firstName: 'react', lastName: 'redux' },
+                };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
           }}
         >
           change name
