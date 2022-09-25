597946bc8747d22a2f6234395f69e1ab8dd0f8d1
”0xAureoleNode“ <aureolenode@gmail.com>
2022-09-25 22:53:30 +0800

    styling the cardlist component & search-box

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,13 +1,25 @@
 import { Component } from 'react';
+import './card-list.styless.css';
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
+
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h4 key={monster.id}>{monster.name}</h4>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { id, name, email } = monster;
+          return (
+            <div className="card-container" key={id}>
+              <img
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+              />
+              <h4>{name}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
