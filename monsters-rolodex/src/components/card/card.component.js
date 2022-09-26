c4a04b173edda70b0e0f34709d4484b181ed6c80
”0xAureoleNode“ <aureolenode@gmail.com>
2022-09-26 15:15:17 +0800

    commit for add other updates about the functional changes

--- monsters-rolodex/src/components/card/card.component.js
+++ monsters-rolodex/src/components/card/card.component.js
@@ -1,20 +1,17 @@
-import { Component } from 'react';
 import './card.styless.css';
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-          alt={`monster ${name}`}
-        />
-        <h4>{name}</h4>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt={`monster ${name}`}
+      />
+      <h4>{name}</h4>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
